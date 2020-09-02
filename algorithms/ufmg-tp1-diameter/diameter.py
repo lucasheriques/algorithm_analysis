d39e5e3eac032e2784c94ecf83ac9cce4230a5ff
Lucas Henrique <lucasheriques@gmail.com>
2020-09-02 17:05:53 -0300

    Finishing up diameter problem

--- algorithms/ufmg-tp1-diameter/diameter.py
+++ algorithms/ufmg-tp1-diameter/diameter.py
@@ -1,11 +1,31 @@
 import time
+import argparse
+import os
+from argparse import ArgumentParser
+
+
+def validate_file(f):
+    if not os.path.exists(f):
+        # Argparse uses the ArgumentTypeError to give a rejection message like:
+        # error: argument input: x does not exist
+        raise argparse.ArgumentTypeError("{0} does not exist".format(f))
+    return f
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser(description="Read file form Command line.")
+    parser.add_argument("-i", "--input", dest="filename", required=True, type=validate_file,
+                        help="input file", metavar="FILE")
+    args = parser.parse_args()
+    print(args.filename)
 
 G = None
 nV = edges = 0
-INF = 999
+INF = 999999
 
 startInput = time.time()
-with open(r"E:\projects\algorithm_analysis\algorithms\ufmg-tp1-diameter\G-300-56691-densidade-0.63.txt", "r") as file:
+with open(args.filename, "r") as file:
     stripped_line = file.readline().strip().split(" ")
     nV = int(stripped_line[0])
     G = [[0 if i == j else INF for j in range(nV)] for i in range(nV)]
@@ -42,12 +62,11 @@ def diameter_vertices(G):
         for j in range(nV):
             if G[i][j] != INF and G[i][j] > max_distance:
                 max_distance = G[i][j]
-                vertices = [i + 1, j + 1]
+                vertices = [i, j]
 
     return max_distance, "{0} {1}".format(vertices[0], vertices[1])
 
 
-# Printing the solution
 def print_solution(distance):
     for i in range(nV):
         for j in range(nV):
@@ -58,13 +77,75 @@ def print_solution(distance):
         print(" ")
 
 
+def min_distance(dist, visited_vertices):
+    # Initilaize minimum distance for next node
+    min = INF
+
+    # Search not nearest vertex not in the
+    # shortest path tree
+    for v in range(len(dist)):
+        if dist[v] < min and visited_vertices[v] == False:
+            min = dist[v]
+            min_index = v
+
+    return min_index
+
+
+def printPath(parent, j, path=[]):
+
+    # Base Case : If j is source
+    if parent[j] == -1:
+        path.append(j+1)
+        return
+    printPath(parent, parent[j])
+    path.append(j+1)
+
+    return path
+
+
+def printSolution(dist, parent, src, target, path):
+    print("Vertex \t\tDistance from Source\tPath")
+    print("\n%d --> %d \t\t%d\t\t" % (src, target, dist[target]), end=" ")
+    print(path, end=" ")
+
+
+def dijkstra(G, source, target):
+    dist = [INF] * len(G)
+    parent = [-1] * len(G)
+    visited_vertices = [False] * len(G)
+    dist[source] = 0
+
+    for _ in range(len(G)):
+        u = min_distance(dist, visited_vertices)
+
+        visited_vertices[u] = True
+
+        for i in range(len(G)):
+            if G[u][i] and visited_vertices[i] == False and dist[i] > dist[u] + G[u][i]:
+                dist[i] = dist[u] + G[u][i]
+                parent[i] = u
+
+        if visited_vertices[target] == True:
+            break
+
+    path = printPath(parent, target)
+
+    return path
+
+
 trueStart = time.time()
 distances = floyd_warshall(G)
 diameter, vertices = diameter_vertices(distances)
+vertices = vertices.split(" ")
+vertices = [int(i) for i in vertices]
+path = dijkstra(G, vertices[0], vertices[1])
 trueEnd = time.time()
+
 
 trueTime = trueEnd - trueStart + inputTime
 
-print("{0}\n{1}\n{2}\n{3}".format(diameter, vertices, 0, 0))
+
+print("{0}\n{1} {2}\n{3}\n{4}".format(
+    diameter, vertices[0] + 1, vertices[1] + 1,  len(path), " ".join(str(x) for x in path)))
 
 print("Time to run: {:03.2f} seconds".format(trueTime))
